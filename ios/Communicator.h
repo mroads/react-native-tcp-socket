d55767a17ba94fa80aa0a223bd27edb4f075ef55
User <user@vijay.local>
2020-08-07 20:28:04 +0530

    Added ios library

---
+++ ios/Communicator.h
@@ -0,0 +1,71 @@
+/*
+ * tcpconnect.h
+ * TCPHelper
+ *
+ * Created by Árpád Goretity on 30/12/2011.
+ * Released into the public domain,
+ * with no warranty of any kind
+ */
+
+#ifndef TCPCONNECT_H
+#define TCPCONNECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+/*
+  Returns a socket file descriptor,
+  from/to which one can read() or write().
+  To be close()'d after use.
+  Useful for clients wanting to connect to a specific server
+  The server must be already listening using connect_to_client().
+  This function does not return until a server
+  accepted the connection on the specified port.
+*/
+
+int tcpconnect_start_client(const char *hostname, const char *port);
+
+/*
+  Returns a socket file descriptor,
+  from/to which one can read() or write().
+  To be close()'d after use.
+  Useful for servers wanting to accept an incoming
+  connection from any client.
+  This function does not return until a client
+  has successfully connected on the specified port.
+*/
+
+int tcpconnect_start_server(const char *port);
+
+/*
+  Returns a file descriptor, which can be supplied to tcpconnect_accept_single
+  in order to retrieve a read()able-write()able socket file descriptor.
+*/
+
+int tcpconnect_start_multiple(const char *port);
+
+/*
+  Takes a socket file descriptor returned by tcpconnect_start_multiple().
+  This blocks until a client connects to the other end of the socket.
+  Returns a readable-writable socket file descriptor on success or -1 on error.
+*/
+
+int tcpconnect_accept_single(int stubfd);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* TCPCONNECT_H */
+
--- ios/TcpSocket.m
+++ ios/TcpSocket.m
@@ -1,14 +1,49 @@
 #import "TcpSocket.h"
+#import <React/RCTLog.h>
+#import "Communicator.h"
 
 
 @implementation TcpSocket
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(sendDataToSocket:(NSString *)url port:(NSString *)port bytes:(NSString *)data callback:(RCTResponseSenderBlock)callback)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  RCTLogInfo(@"url: %@ port: %@ bytes: %@", url, port, data);
+  int sockfd = tcpconnect_start_client([url UTF8String], [port UTF8String]);
+  RCTLogInfo(@"sockfd: %d ", sockfd);
+  NSData *bytes = [data dataUsingEncoding:NSUTF8StringEncoding];
+  
+  if(sockfd<0){
+    callback(@[@"error connecting to socket"]);
+    return;
+  }
+
+  
+  ssize_t length = bytes.length;
+
+  const char *buf = bytes.bytes;
+  
+  int chunkSize = 20;
+
+  while (length) {
+    // Be prepared to non-blocking sockets
+    size_t sendlen = length < chunkSize ? length : chunkSize;
+    ssize_t len_written = write(sockfd, buf, sendlen);
+
+    if (len_written < 0) {
+      // error
+      callback(@[@"data is empty"]);
+      return;
+    }
+
+    length -= len_written;
+    buf += len_written;
+  }
+  
+  callback(@[[NSNull null], @"success"]);
+  
+  close(sockfd);
 }
 
 @end
